734f2b7f42fe858587274ecbd8c378b9d592ef64
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-22 15:25:05 +0000

    address fixed

--- src/components/dashboard/dashboardContents/userManagementSec.tsx
+++ src/components/dashboard/dashboardContents/userManagementSec.tsx
@@ -4,7 +4,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import "./dashboard-styles.css";
+
+import "../../../assets/css/dashboard-styles.css";
 
 import Swal from "sweetalert2";
 import axios from "axios";
