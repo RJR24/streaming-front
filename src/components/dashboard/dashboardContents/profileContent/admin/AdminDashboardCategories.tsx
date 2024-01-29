4d0d3a8e031af796195f66b7da716f2a45a7cdcd
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-29 15:49:41 +0000

    url amended

--- src/components/dashboard/dashboardContents/profileContent/admin/AdminDashboardCategories.tsx
+++ src/components/dashboard/dashboardContents/profileContent/admin/AdminDashboardCategories.tsx
@@ -191,7 +191,7 @@ const AdminDashboardMain = () => {
                   </div>
                 </Link>
                 <Link
-                  href={"/admin/main"}
+                  href={"/admin/categories"}
                   className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group cursor-pointer"
                 >
                   <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
