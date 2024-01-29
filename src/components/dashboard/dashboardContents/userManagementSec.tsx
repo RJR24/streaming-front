ffdd7a19840179de35c1f3944551b29302eca5e7
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-29 00:05:06 +0000

    updated

--- src/components/dashboard/dashboardContents/userManagementSec.tsx
+++ src/components/dashboard/dashboardContents/userManagementSec.tsx
@@ -29,11 +29,9 @@ const UsersManagement = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   
-  // const [showResetPassword, setShowResetPassword] = useState(false);
   const [showUserInfoModal, setShowUserInfoModal] = useState(false);
   const [userEmail, setUserEmail] = useState<string>("");
   const [userId, setUserId] = useState<string>("");
-  // const [userName, setUserName] = useState<string>("");
   const [users, setUsers] = useState<User[]>([]);
   const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
   const router = useRouter();
@@ -254,8 +252,8 @@ const UsersManagement = () => {
 
         <div className="BgImage antialiased bg-black bg-opacity-80 w-full min-h-screen text-slate-300 relative py-4">
           <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
-            <div id="menu" className="bg-white/10 col-span-3 rounded-lg p-4 ">
-              <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 to-transparent bg-clip-text text-transparent">
+            <div id="menu" className="bg-white/10 col-span-3 rounded-lg p2 md:p-4 ">
+              <h1 className="font-bold text-base lg:text-3xl bg-gradient-to-br from-white via-white/50 to-transparent bg-clip-text text-transparent">
                 Dashboard<span className="text-indigo-400">.</span>
               </h1>
               <p className="text-slate-400 text-sm mb-2">Welcome</p>
@@ -459,7 +457,6 @@ const UsersManagement = () => {
           <table className="w-full whitespace-nowrap">
             <thead className="bg-black/60">
               <th className="text-left py-3 px-2 rounded-l-lg">Name</th>
-              <th className="text-left py-3 px-2">Username</th>
               <th className="text-left py-3 px-2">Email</th>
               <th className="text-left py-3 px-2">Admin</th>
               <th className="text-left py-3 px-2 rounded-r-lg">Actions</th>
@@ -469,13 +466,10 @@ const UsersManagement = () => {
                 <tr key={user._id} className="border-b border-gray-700">
                   <td className="py-3 px-2 font-bold">
                     <div className="inline-flex space-x-3 items-center">
-                      <span>
-                        
-                      </span>
+                     
                       <span>{user.name}</span>
                     </div>
                   </td>
-                  <td className="py-3 px-2">{user.name}</td>
                   <td className="py-3 px-2">{user.email}</td>
                   <td className="py-3 px-2">
                     {user.isAdmin ? "✅Yes" : "⛔No"}
@@ -544,99 +538,3 @@ const UsersManagement = () => {
 };
 
 export default UsersManagement;
-
-
-
-
-
-
-
-
-// const UsersManagement: React.FC = () => {
-//   const [users, setUsers] = useState<User[]>([]);
-//   const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
-
-  
-//   return (
-//     <div className="bg-white/10 col-span-9 rounded-lg p-6">
-//       <div className="overflow-hidden">
-//         <h1 className="text-3xl font-bold mb-6">Users Management</h1>
-//         <div className="mt-4">
-//           <h2 className="text-2xl font-bold mb-4">Users List</h2>
-//           <table className="w-full whitespace-nowrap">
-//             <thead className="bg-black/60">
-//               <th className="text-left py-3 px-2 rounded-l-lg">Name</th>
-//               <th className="text-left py-3 px-2">Username</th>
-//               <th className="text-left py-3 px-2">Email</th>
-//               <th className="text-left py-3 px-2">Admin</th>
-//               <th className="text-left py-3 px-2 rounded-r-lg">Actions</th>
-//             </thead>
-//             <tbody>
-//               {users.map((user) => (
-//                 <tr key={user._id} className="border-b border-gray-700">
-//                   <td className="py-3 px-2 font-bold">
-//                     <div className="inline-flex space-x-3 items-center">
-//                       <span>
-                        
-//                       </span>
-//                       <span>{user.name}</span>
-//                     </div>
-//                   </td>
-//                   <td className="py-3 px-2">{user.name}</td>
-//                   <td className="py-3 px-2">{user.email}</td>
-//                   <td className="py-3 px-2">
-//                     {user.isAdmin ? "✅Yes" : "⛔No"}
-//                   </td>
-//                   <td className="py-3 px-2">
-//                     <div className="inline-flex items-center space-x-3">
-//                       <p title="Edit" className="cursor-pointer hover:text-blue-500">
-//                         <svg
-//                           xmlns="http://www.w3.org/2000/svg"
-//                           fill="none"
-//                           viewBox="0 0 24 24"
-//                           strokeWidth="1.5"
-//                           stroke="currentColor"
-//                           className="w-5 h-5"
-//                         >
-//                           <path
-//                             strokeLinecap="round"
-//                             strokeLinejoin="round"
-//                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-//                           />
-//                         </svg>
-//                       </p>
-//                       <button
-//                         title="Suspend-user"
-//                         className="hover:text-red-500"
-//                         onClick={() => suspendUser(user._id)}
-//                       >
-//                         <svg
-//                           xmlns="http://www.w3.org/2000/svg"
-//                           fill="none"
-//                           viewBox="0 0 24 24"
-//                           strokeWidth="1.5"
-//                           stroke={
-//                             selectedUserId === user._id ? "red" : "currentColor"
-//                           }
-//                           className="w-5 h-5"
-//                         >
-//                           <path
-//                             strokeLinecap="round"
-//                             strokeLinejoin="round"
-//                             d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-//                           />
-//                         </svg>
-//                       </button>
-//                     </div>
-//                   </td>
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
