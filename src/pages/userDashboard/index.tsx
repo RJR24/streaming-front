df02ea6355ac6ecf95bf432a9f54545ff26eee48
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-29 13:43:46 +0000

    deleted

--- src/pages/userDashboard/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UserDashboard from "../../components/dashboard/AdminDashboard";
-
-const Dashboard = () => {
-  return <UserDashboard />;
-};
-
-export default Dashboard;
