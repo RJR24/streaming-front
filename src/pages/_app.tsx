03d1009904455c711e891793415721099d2b8fc7
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-29 18:18:32 +0000

    conditionally render the navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+
 import "../assets/css/globals.css";
 import "../assets/css/changePassword-style.css";
 import "../assets/css/dashboard-styles.css";
@@ -6,15 +11,18 @@ import "../assets/css/login-styles.css";
 import "../assets/css/navbar-styles.css";
 import "../assets/css/signup-styles.css";
 import "../assets/css/hero-styles.css";
-import Footer from "../components/footer/Footer";
-import Navbar from "../components/navbar/Navbar";
 
-const App = ({ Component, pageProps }) => {
-  return(
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  const hideNavbarOnPages = ["/login", "/signup"];
+
+  // Check if the current route is in the hideNavbarOnPages array
+  const shouldHideNavbar = hideNavbarOnPages.includes(router.pathname);
+  return (
     <>
-    <Navbar />
-     <Component {...pageProps} />
-     <Footer />
+      {!shouldHideNavbar && <Navbar />}
+      <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
