292f6942aa348308603b0ce3ed1e8a1a90690876
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-29 18:03:01 +0000

    user profile page created

---
+++ src/pages/userDashboard/UserProfile.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserProfile from "../../components/dashboard/dashboardContents/profileContent/user/UserProfile";
+
+const profile = () => {
+  return <UserProfile />;
+};
+
+export default profile;
