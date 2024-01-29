5a450499996d70071c10754c88cdc8c10b80762f
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-29 14:31:53 +0000

    deploy

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import UserDashboard from "../../components/dashboard/UserDashboard";
-import Navbar from "../../components/navbar/Navbar";
 
 const Dashboard = () => {
   return (
