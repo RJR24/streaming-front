975bdc87a7e8d01cb8d16c772181dcfee8f9e163
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-31 21:05:40 +0000

    class name changed

--- src/components/dashboard/changePassword/ChangePasswordForm.tsx
+++ src/components/dashboard/changePassword/ChangePasswordForm.tsx
@@ -70,11 +70,11 @@ const ResetPassword = () => {
   return (
     <section className="">
       
-      <div className="login flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="change-password-email flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-black opacity-70 text-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-600 dark:border-gray-700">
           <div className="px-12 py-[4.25rem] space-y-4 md:space-y-6 ">
             <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl dark:text-white">
-              Reset Password
+              Password Reset
             </h1>
             <form
               className=" form-styles space-y-4"
