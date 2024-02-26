cfaf06a9a6f882b40f01601fe19587f0e4a58c2a
Kaveh <rezarezaeijamiuk@gmail.com>
2024-02-26 14:18:45 +0000

    logout bug fixed by changing Link to div!

--- src/components/dashboard/dashboardContents/profileContent/admin/AdminDashboardProfile.tsx
+++ src/components/dashboard/dashboardContents/profileContent/admin/AdminDashboardProfile.tsx
@@ -269,11 +269,11 @@ const AdminDashboardProfile = () => {
                     </div>
                   </div>
                 </Link>
-                <Link
-                  href={"/login"}
-                  className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group"
-                >
-                  <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
+                <div className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group cursor-pointer">
+                  <div
+                    className="flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center"
+                    onClick={handleLogout}
+                  >
                     <div>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -295,7 +295,7 @@ const AdminDashboardProfile = () => {
                         />
                       </svg>
                     </div>
-                    <div onClick={handleLogout}>
+                    <div>
                       <p className="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">
                         Logout
                       </p>
@@ -304,7 +304,7 @@ const AdminDashboardProfile = () => {
                       </p>
                     </div>
                   </div>
-                </Link>
+                </div>
               </div>
               <p className="text-sm text-center text-gray-600">
                 v1.0.0 | &copy; 2024 Kaveh RezaeiJAmi
